fc2a448121ddb9bb95ffd1c32c1f6f7ac3e098da
yvcode <36304957+yvcode@users.noreply.github.com>
2026-01-17 21:28:11 +0200

    Added: Encoding program

---
+++ Encode-Pythagorean-Triples.cpp
@@ -0,0 +1,34 @@
+#include <fstream> 
+#include <iostream> 
+#include <cmath>
+#include <string>
+
+
+int main() {
+    int PTN_NUMBER = 7824;
+    int line_count = 0;
+    std::string cnf_formula = "";
+    std::ofstream outputFile("ptn.cnf");
+    
+
+    for (int a = 1; a <= PTN_NUMBER; ++a) {
+        for (int b = a; b <= PTN_NUMBER; ++b) {
+            int c_squared = a * a + b * b;
+            double c = std::sqrt(c_squared);
+            
+            if (c > PTN_NUMBER) {
+                break;
+            }
+            if (c == (int)c) {
+                line_count+=2;
+                cnf_formula += std::to_string(a) + " " + std::to_string(b) + " " + std::to_string((int)c) + " 0\n";
+                cnf_formula += std::to_string(-a) + " " + std::to_string(-b) + " " + std::to_string(-(int)c) + " 0\n";
+    }
+        }
+    }
+    outputFile << "p cnf "+ std::to_string(PTN_NUMBER) + " " + std::to_string(line_count) << std::endl;
+    outputFile << cnf_formula;
+    outputFile.close();
+    
+    return 0;
+}
