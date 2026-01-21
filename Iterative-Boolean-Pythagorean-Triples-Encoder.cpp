a772171b2c3e755fc029fbf974f9f45cb7eff50a
yvcode <36304957+yvcode@users.noreply.github.com>
2026-01-21 23:04:43 +0200

    Update Iterative-Boolean-Pythagorean-Triples-Encoder.cpp

--- Iterative-Boolean-Pythagorean-Triples-Encoder.cpp
+++ Iterative-Boolean-Pythagorean-Triples-Encoder.cpp
@@ -8,7 +8,6 @@ void create_ptn_file(int PTN_NUMBER)
     int line_count = 0;
     std::string cnf_formula = "";
     std::ofstream outputFile("ptn"+ std::to_string(PTN_NUMBER) +".cnf");
-    
 
     for (int a = 1; a <= PTN_NUMBER; ++a) {
         for (int b = a; b <= PTN_NUMBER; ++b) {
@@ -22,15 +21,19 @@ void create_ptn_file(int PTN_NUMBER)
                 line_count+=2;
                 cnf_formula += std::to_string(a) + " " + std::to_string(b) + " " + std::to_string((int)c) + " 0\n";
                 cnf_formula += std::to_string(-a) + " " + std::to_string(-b) + " " + std::to_string(-(int)c) + " 0\n";
-    }
+            }
         }
     }
+    
     outputFile << "p cnf "+ std::to_string(PTN_NUMBER) + " " + std::to_string(line_count) << std::endl;
     outputFile << cnf_formula;
     outputFile.close();
 }
-int main() {
-    for(int i=0; i<7000;i++){
+
+int main() 
+{
+    for(int i=0; i<7000;i++)
+    {
         create_ptn_file(i);
     }
     
